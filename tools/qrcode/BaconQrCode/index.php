dc5bc8ccd3bbf94481e9cf8cff4c0052d896450a
chuongnv <vanchuong201>
2017-12-04 20:48:14 +0700

    delete thua`

--- tools/qrcode/BaconQrCode/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$renderer = new \BaconQrCode\Renderer\Image\Png();
-$renderer->setHeight(256);
-$renderer->setWidth(256);
-$writer = new \BaconQrCode\Writer($renderer);
-$writer->writeFile('Hello World!', 'qrcode.png');
-?>
